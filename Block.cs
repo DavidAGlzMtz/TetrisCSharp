a809f84fa94bca436f18bb295a9de769a33f666a
DavidAGlzMtz <david.agzmz@gmail.com>
2023-10-23 20:58:29 -0600

    Creating code
    Blocks code
    GameGrid code
    GameState code

---
+++ Block.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisCSharp {
+    public abstract class Block {
+        protected abstract Position[][] Tiles { get; }
+        protected abstract Position StartOffset { get; }
+        public abstract int ID { get; }
+
+        private int rotationState;
+        private Position offset;
+
+        public Block() {
+            offset = new Position(StartOffset.Row, StartOffset.Col);
+        }
+
+        public IEnumerable<Position> TilePositions() {
+            foreach (Position p in Tiles[rotationState]) {
+                yield return new Position(p.Row + offset.Row, p.Col + offset.Col);
+            }
+        }
+
+        public void RotateCW() {
+            rotationState = (rotationState + 1) % Tiles.Length;
+        }
+
+        public void RotateCCW() {
+            if (rotationState == 0)
+                rotationState = (rotationState - 1);
+            else
+                rotationState--;
+        }
+
+        public void Move(int rows, int cols) {
+            offset.Row += rows;
+            offset.Col += cols;
+        }
+
+        public void Reset() {
+            rotationState = 0;
+            offset.Row = StartOffset.Row;
+            offset.Col = StartOffset.Col;
+        }
+    }
+}
